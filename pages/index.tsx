70c050c41d3415c8c1fd92b8a3a276c8f28f1dc1
Adel Aziz Boulfekhar <boulfekhar.adelaziz@gmail.com>
2022-06-25 11:27:51 +0100

    added some tests

--- pages/index.tsx
+++ pages/index.tsx
@@ -160,14 +160,7 @@ const IndexPage = () => {
         </Link>
       </main>
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+        <p>Tests made using Jest</p>
       </footer>
     </div>
   );
